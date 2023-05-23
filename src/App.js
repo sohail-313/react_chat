e80d1bded64d02b852dd52e208d52ebf6b3bb51f
Mohammed Sohail <85786620+sohail-313@users.noreply.github.com>
2023-05-23 23:07:23 +0530

    Links updated

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Chat from './Chat'
 import io from "socket.io-client"
 import { useState } from "react"
-const socket = io.connect("http://localhost:3001");
+const socket = io.connect("https://chat-app-0kp9.onrender.com/");
 
 function App() {
   const [user, setUserName] = useState("");
